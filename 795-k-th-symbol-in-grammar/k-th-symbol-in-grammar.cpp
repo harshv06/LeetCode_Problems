502328db16786e0f2c8fd51226861c1c2cd3b5b3
Harsh VIshwakarma <158333362+harshv06@users.noreply.github.com>
2026-01-30 11:47:04 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (79.23%) - LeetSync

--- 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.cpp
+++ 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
-    int solve(int n, int k){
-        if(n == 1 && k == 1) return 0;
-
-        // check k is on right side or left side
-        int totalSize = pow(2, n-1);
-        int leftDigits = totalSize/2;
-        if(k<= leftDigits){
-            return solve(n-1, k);
-        } 
-
-        // rightSize
-        return 1 + solve(n-1, k-leftDigits);
-    }
-
     int kthGrammar(int n, int k) {
         int times = 0;
-        times = solve(n, k);
+
+        if(n==1) return 0;
+
+        while(n!=1){
+            int totalSize = pow(2, n-1);
+            int leftDigits = totalSize/2;
+            if (k > leftDigits) {
+                times++;
+                k = k - leftDigits;
+            }
+            n--;
+        }
+
         return times % 2 == 0 ? 0 : 1; 
     }
 
